0a8d3c181761d8dc7352e5ce6abba9749dee388e
Minhaz <sharif35-3001@diu.edu.bd>
2022-09-24 21:22:54 +0600

    fix: mailto fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 					&copy; 2022 <a href="https://github.com/Sharif-Minhaz">Minhaz</a>{" "}
 					<span> | </span> All rights reserved <span> | </span>
 				</div>
-				<a href="mailto: sharif35-3001@diu.edu.bd&Subject=report an error">
+				<a href="mailto: sharif35-3001@diu.edu.bd?Subject=Report an error">
 					<span> report an error</span>
 				</a>
 			</div>
